e36e1bd8a4ec6b6713a7941740e3827eb3ce9b40
BootprojectStore <bootproject@icloud.com>
2018-02-22 11:02:31 +0100

    Add project files.

---
+++ TextTalk.App/TextTalk.App/DataViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using UIKit;
+
+namespace TextTalk.App
+{
+	public partial class DataViewController : UIViewController
+	{
+		public string DataObject {
+			get;
+			set;
+		}
+
+		protected DataViewController (IntPtr handle) : base (handle)
+		{
+            this.Add(new MultiStoreView());
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+			dataLabel.Text = DataObject;
+		}
+	}
+
+    public class MultiStoreView : BaseView
+    {
+        public override void ChangeView() { }
+    }
+
+
+    public abstract class BaseView : UIView
+    {
+        protected BaseView()
+        {
+        }
+
+        public abstract void ChangeView();
+    }
+
+
+}
+
